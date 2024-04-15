15bf5389d788191e42318f26cc4c8993252493ba
LuciaNishimiya <lucianishimiya@gmail.com>
2024-04-15 03:05:57 -0400

    [ADD] test store

--- frontend/src/pages/ProductDetails/index.jsx
+++ frontend/src/pages/ProductDetails/index.jsx
@@ -1,7 +1,57 @@
 import { useParams } from "react-router-dom";
+import { useState, useEffect } from "react";
+import { 
+    SetWebstoreIdentifier,
+    GetCategories,
+    GetCategory,
+    GetPackages,
+    GetPackage,
+    CreateMinecraftBasket,
+    GetBasket 
+  } from "tebex_headless";
 const ProductDetails = () =>{
+    const [Package, setPackage] = useState([]);
     const params = useParams();
-    return <h1>ProductDetails: {params.prodcutId}</h1>;
+    
+    const fetchCategories = async () => {
+      try {
+        const PackageData = await GetPackage(params.prodcutId);
+        console.log(PackageData)
+        setPackage(PackageData);
+      } catch (error) {
+        console.error("Error fetching categories:", error);
+        throw error;
+      }
+    };
+  
+    useEffect(() => {
+      fetchCategories();
+    }, []);
+
+
+
+
+
+
+
+
+
+    return (
+        <aside className=''>
+            <figure className=''>
+                <figcaption className=''>
+                    {Package.name}
+                </figcaption>
+                <figcaption className='font-medium text-xl'>
+                    {Package.total_price}
+                </figcaption>
+            </figure>
+
+            <p className="my-16">
+                {Package.description}
+            </p>
+        </aside>
+      );
 }
 
 export default ProductDetails;
--- frontend/src/pages/Shop/index.jsx
+++ frontend/src/pages/Shop/index.jsx
@@ -1,18 +1,83 @@
-import {NavLink} from "react-router-dom";
-const Products = () =>{
- return <nav>
-        <ul>
-          <li>
-            <NavLink to="/products/1">Products 1</NavLink>
+import './styles.css'
+import {
+  SetWebstoreIdentifier,
+  GetCategories,
+  GetCategory,
+  GetPackages,
+  GetPackage,
+  CreateMinecraftBasket,
+  GetBasket
+} from "tebex_headless";
+import { useState, useEffect } from "react";
+import { Link } from 'react-router-dom';
+SetWebstoreIdentifier("l9j9-cf13199dfe8cadbcdbb253df6ad3b7c001042512");
+
+const Products = () => {
+  const [categories, setCategories] = useState([]);
+  const fetchCategories = async () => {
+    try {
+      const categoriesData = await GetCategories(false);
+      setCategories(categoriesData);
+    } catch (error) {
+      console.error("Error fetching categories:", error);
+      throw error;
+    }
+  };
+
+  useEffect(() => {
+    fetchCategories();
+  }, []);
+
+
+
+
+  //////////////////////
+  const [packages, setPackages] = useState([]);
+
+  useEffect(() => {
+    const fetchPackages = async () => {
+      try {
+        const packageData = await GetPackages();
+        setPackages(packageData);
+        console.log(packageData)
+      } catch (error) {
+        console.error("Error fetching packages:", error);
+      }
+    };
+
+    fetchPackages();
+  }, []);
+  ///////////////////
+
+
+
+
+
+  return (
+    <section className='categories-section'>
+      <ul>
+        {categories.map((category) => (
+          <li key={category.id}>
+            <button className='category-button'>{category.name}</button>
           </li>
-          <li>
-            <NavLink to="/products/2">Products 2</NavLink>
-          </li>
-          <li>
-            <NavLink to="/products/3">Products 3</NavLink>
-          </li>
-        </ul>
-    </nav>;
-}
+        ))}
+      </ul>
+      <div className="product-grid">
+        {packages.map((packageItem) => (
+          <div key={packageItem.id} className="product-item">
+            <Link to={`./${packageItem.id}`}>
+              <img className='product-image' src={packageItem.image} alt="" />
+              <h5>{packageItem.name}</h5>
+            </Link>
+            <div className='product-sale'>
+              <p>{packageItem.total_price} {packageItem.currency}</p>
+              <Link to={`./${packageItem.id}`}>Comprar</Link>
+            </div>
+          </div>
+        ))}
+      </div>
+    </section>
+  );
+};
 
 export default Products;

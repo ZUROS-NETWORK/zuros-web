95b01c163940405da31eb0fde8316808a897cbaf
LuciaNishimiya <lucianishimiya@gmail.com>
2025-03-26 17:43:12 -0600

    Return an error when the cart ID does not exist

--- backend/src/services/cart.ts
+++ backend/src/services/cart.ts
@@ -1,10 +1,14 @@
 export async function getCart(db: D1Database, id: string) {
-    const { results } = await db
-        .prepare(`
+  const cartExists = await db
+    .prepare(`SELECT id FROM cart WHERE id = ?`)
+    .bind(id)
+    .first()
+
+  const { results } = await db
+    .prepare(`
       SELECT  
-        ci.product_id, 
         ci.quantity, 
-        p.id AS product_id, 
+        p.id, 
         p.name, 
         p.short_description, 
         p.base_price, 
@@ -17,10 +21,10 @@ export async function getCart(db: D1Database, id: string) {
       JOIN package p ON ci.product_id = p.id
       WHERE c.id = ?
     `)
-        .bind(id)
-        .all()
+    .bind(id)
+    .all()
 
-    return results.length > 0 ? results : null
+  return cartExists?.id ? results : null
 }
 
 export async function saveCart(db: D1Database, items: any[]) {
@@ -34,58 +38,54 @@ export async function saveCart(db: D1Database, items: any[]) {
     for (const item of items) {
       const productCheck = await db
         .prepare("SELECT id FROM package WHERE id = ?")
-        .bind(item.productId)
+        .bind(item.id)
         .first()
 
       if (!productCheck) {
-        throw new Error(`Product with ID ${item.productId} does not exist.`)
+        throw new Error(`Product with ID ${item.id} does not exist.`)
       }
       await db
         .prepare("INSERT INTO cart_items (cart_id, product_id, quantity) VALUES (?, ?, ?)")
-        .bind(cartId, item.productId, item.quantity)
+        .bind(cartId, item.id, item.quantity)
         .run()
     }
     return cartId
-  } catch (error:any) {
+  } catch (error: any) {
     throw new Error("An error occurred while saving the cart:" + error.message)
   }
 }
 
 
 export async function modifyCart(db: D1Database, id: string, items: any[]) {
-    for (const item of items) {
-        const existingItem = await db
-            .prepare("SELECT * FROM cart_items WHERE cart_id = ? AND product_id = ?")
-            .bind(id, item.productId)
-            .first();
-        if (existingItem) {
-            await db
-                .prepare("UPDATE cart_items SET quantity = ? WHERE cart_id = ? AND product_id = ?")
-                .bind(item.quantity, id, item.productId)
-                .run();
-        } else {
-            await db
-                .prepare("INSERT INTO cart_items (cart_id, product_id, quantity) VALUES (?, ?, ?)")
-                .bind(id, item.productId, item.quantity)
-                .run();
-        }
-    }
-
-    const productIds = items.map(item => item.productId);
-
-    await db
-        .prepare("DELETE FROM cart_items WHERE cart_id = ? AND product_id NOT IN (" + productIds.map(() => "?").join(",") + ")")
-        .bind(id, ...productIds)
+  for (const item of items) {
+    const existingItem = await db
+      .prepare("SELECT * FROM cart_items WHERE cart_id = ? AND product_id = ?")
+      .bind(id, item.id)
+      .first();
+    if (existingItem) {
+      await db
+        .prepare("UPDATE cart_items SET quantity = ? WHERE cart_id = ? AND product_id = ?")
+        .bind(item.quantity, id, item.id)
         .run();
+    } else {
+      await db
+        .prepare("INSERT INTO cart_items (cart_id, product_id, quantity) VALUES (?, ?, ?)")
+        .bind(id, item.id, item.quantity)
+        .run();
+    }
+  }
 
-    return true;
+  const ids = items.map(item => item.id);
+
+  await db
+    .prepare("DELETE FROM cart_items WHERE cart_id = ? AND product_id NOT IN (" + ids.map(() => "?").join(",") + ")")
+    .bind(id, ...ids)
+    .run();
+
+  return true;
 }
 
-
-
-
-
 export async function removeCart(db: D1Database, id: string) {
-    const result = await db.prepare("DELETE FROM cart WHERE id = ?").bind(id).run()
-    return result.meta.changes > 0
+  const result = await db.prepare("DELETE FROM cart WHERE id = ?").bind(id).run()
+  return result.meta.changes > 0
 }

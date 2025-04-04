a062d21f3a6e05ea3c73a08462e80f0eb9b3f17f
Lucia Nishimiya <lucianishimiya@gmail.com>
2025-04-04 04:31:30 -0600

    Fix vite.config.js

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,13 +1,8 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react-swc'
-import sitemapPlugin from 'vite-plugin-sitemap'
-
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [
-    react(),
-    sitemapPlugin({
-       hostname: "https://tusitio.com",
-     }),
+    react()
   ],
 })
